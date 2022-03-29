46e24c1e6d0aa34ad4ce25c2ad4cfaeadfa8b875
Vuong Phan <huuwulleop@hotmail.com>
2022-03-28 23:26:58 -0400

    Add boilerplate code

--- game.py
+++ game.py
@@ -1,1 +1,25 @@
-import pygame
+import pygame, sys
+
+# Setup
+pygame.init()
+clock = pygame.time.Clock()
+
+# Setup window
+screen_width = 1280
+screen_heigth = 960
+screen = pygame.display.set_mode((screen_width, screen_heigth))
+pygame.display.set_caption("Pong")
+
+
+
+
+# Main game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    # Update the window
+    pygame.display.flip()
+    clock.tick(60) # Limit the game to 60FPS
