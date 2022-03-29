8d51e45172d3f5ebc96af67215a50ac051e5a6b1
Vuong Phan <huuwulleop@hotmail.com>
2022-03-29 00:28:32 -0400

    Fix typos and refactor player

--- game.py
+++ game.py
@@ -5,13 +5,21 @@ def ball_animation():
     ball.x += ball_speed_x
     ball.y += ball_speed_y
     
-    if ball.top <= 0 or ball.bottom >= screen_heigth:
+    if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
     if ball.left <= 0 or ball.right >= screen_width:
         ball_speed_x *= -1
     
     if ball.colliderect(player) or ball.colliderect(opponent):
         ball_speed_x *= -1
+
+
+def player_animation():
+    player.y += player_speed
+    if player.top <= 0:
+        player.top = 0
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
     
 
 # Setup
@@ -20,16 +28,16 @@ clock = pygame.time.Clock()
 
 # Setup window
 screen_width = 1280
-screen_heigth = 960
-screen = pygame.display.set_mode((screen_width, screen_heigth))
+screen_height = 960
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
 ball_w = 30
 ball_h = 30
 
-ball = pygame.Rect(screen_width/2 - ball_w/2, screen_heigth/2 - ball_h/2, ball_w, ball_h)
-player = pygame.Rect(screen_width - 20, screen_heigth/2 - 70, 10, 140)
-opponent = pygame.Rect(10, screen_heigth/2 - 70, 10, 140)
+ball = pygame.Rect(screen_width/2 - ball_w/2, screen_height/2 - ball_h/2, ball_w, ball_h)
+player = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
+opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
 bg_color = pygame.Color("grey12")
 light_grey = (200, 200, 200)
@@ -37,6 +45,7 @@ light_grey = (200, 200, 200)
 ball_speed_x = 7
 ball_speed_y = 7
 player_speed = 0
+opponent_speed = 0
 
 # Main game loop
 while True:
@@ -57,14 +66,14 @@ while True:
                 player_speed += 7
     
     ball_animation()
-    player.y += player_speed
+    player_animation()
     
     # Draw rectangles
     screen.fill(bg_color)
     pygame.draw.rect(screen, light_grey, player)
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
-    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_heigth))
+    pygame.draw.aaline(screen, light_grey, (screen_width/2, 0), (screen_width/2, screen_height))
     
     # Update the window
     pygame.display.flip()
